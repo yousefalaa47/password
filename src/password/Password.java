6ccf790f340577806670d0d2b97a09520beaea4f
yousefalaa47 <86966633+yousefalaa47@users.noreply.github.com>
2024-11-24 18:11:54 +0200

    Add files via upload

---
+++ src/password/Password.java
@@ -0,0 +1,29 @@
+
+package password;
+import java .util.* ;
+public class Password {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for (long i = 0; i < 1000000000; i++) {
+        int password = 1999 ;
+        int x = input.nextInt();
+        if(password == x){
+            System.out.println("Correct");
+            break ;
+        }
+        
+        else{
+            System.out.println("Wrong");
+            
+            
+           
+        }
+            
+        }
+        
+
+
+   
+    }
+    
+}
